0e789f8694f7899fc6a11d5ee41dcb62cbc72ce5
Fahima Mohammadi <fahimamohammadi@Fahimas-MacBook-Pro.local>
2025-11-04 17:51:12 +1100

    fixed ThankYouPage story issue.

--- src/pages/ThankYouPage.stories.tsx
+++ src/pages/ThankYouPage.stories.tsx
@@ -1,9 +1,17 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import ThankYouPage from "./ThankYouPage";
+import { MemoryRouter } from "react-router-dom";
 
 const meta: Meta<typeof ThankYouPage> = {
   title: "Pages/ThankYouPage",
   component: ThankYouPage,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
 };
 
 export default meta;
